8e1b2e053e5ffcd5fe61176e16f1d48b73d5577e
Raven <hong18s@gmail.com>
2015-09-24 21:27:23 +0900

    Added a timer * timer for main form

--- Pingpong/pingpong.py
+++ Pingpong/pingpong.py
@@ -10,15 +10,23 @@ from PyQt5 import QtGui
 
 import class_ball
 
-#define Main Form class : 약식작성(?)
+
 class Form(QtWidgets.QWidget):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
-        #init
         self.init_all()
+
+        # Setting Timer
+        self.timer = QtCore.QBasicTimer()
+        self.timer.start(1000, self)
+
+    def timerEvent(self, QTimerEvent):
+        pass
+
 
     def init_all(self):
         self.ball = class_ball.Ball(100, 100, 50)
+
 
     def paintEvent(self, QPaintEvent):
         qp = QtGui.QPainter()
@@ -27,13 +35,13 @@ class Form(QtWidgets.QWidget):
         self.ball.draw(QPaintEvent, qp)
         qp.end()
 
-#get instance of MainForm and 프고르램 제어권을 Qt에 전달.
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    # app.processEvents(QtCore.QEventLoop.AllEvents)
     w = Form()
     w.show()
     sys.exit(app.exec())
+
 
 
 
